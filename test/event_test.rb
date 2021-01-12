7b8500f7138d85b38fe9d882594987e3058aacdd
Angel Breaux <abreaux@detaso.com>
2021-01-12 14:35:48 -0600

    Create food truck names test and method

--- test/event_test.rb
+++ test/event_test.rb
@@ -7,14 +7,14 @@ require './lib/event'
 class EventTest < Minitest::Test
   def setup
     @event = Event.new('South Pearl Street Farmers Market')
-    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    @food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    @food_truck3 = FoodTruck.new('Palisade Peach Shack')
 
-    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    @item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
 
     @food_truck1.stock(@item1, 35)
     @food_truck1.stock(@item2, 7)
@@ -36,5 +36,11 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck1)
 
     assert_equal [@food_truck1], @event.food_trucks
+  end
+
+  def test_food_truck_names
+    @event.add_food_truck(@food_truck1)
+
+    assert_equal ['Rocky Mountain Pies'], @event.food_truck_names
   end
 end
